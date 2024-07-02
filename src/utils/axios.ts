028b37546fbae1eefd1f7383f6c5947124f14701
gracebir <grace.devolop@gmail.com>
2024-07-02 11:36:30 +0200

    changing the url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 /** @format */
 import axios from "axios";
-
+// https://task-api-k86t.onrender.com/api/v1
 const instance = axios.create({
     baseURL: "https://task-api-k86t.onrender.com/api/v1",
     // headers: { "X-Custom-Header": "foobar" },
